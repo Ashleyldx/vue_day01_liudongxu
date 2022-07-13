8b354a1dae15d4c47bfe22665403964aed2f7d97
Ashley Liu <ldx363@163.com>
2022-07-13 09:49:45 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+//webpack打包的入口文件
+import Vue from 'vue'; //引入vue
+import App from './App.vue'; //引入组件
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  //构造函数，生成一个实例
+  render: (h) => h(App), //createElement
+  //render渲染app
+}).$mount('#app');
